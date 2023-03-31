ed3e136e2badb9a3b8f37f84ee36711582b1f53c
SACHIN <SACHIN@LAPTOP-64EQKSKD>
2023-03-31 18:25:06 +0530

    Encapsulation1 commit

---
+++ src/trainingcode/launch1.java
@@ -0,0 +1,34 @@
+package trainingcode;
+class Dog
+{
+	private String name;
+	private String color;
+	private int cost;
+
+    void setData(String a,String b,int c) 
+    {
+      name=a;
+      color=b;
+      cost=c;
+    }
+     
+    void getData()
+    {
+    	System.out.println(name);
+    	System.out.println(color);
+    	System.out.println(cost);
+    }
+   
+    }
+
+public class launch1 
+{
+	public static void main(String[] args) 
+	{
+		Dog d1=new Dog();
+		d1.setData("blacky","black",7000);
+		d1.getData();
+		
+	}
+
+}
