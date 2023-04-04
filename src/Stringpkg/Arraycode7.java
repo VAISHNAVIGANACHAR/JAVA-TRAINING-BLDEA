3e7c71eea8a4a1c27c1dd8781f1051d8650fca59
SACHIN <SACHIN@LAPTOP-64EQKSKD>
2023-04-04 19:14:57 +0530

    Array codes commit

--- src/Stringpkg/Arraycode7.java
+++ src/Stringpkg/Arraycode7.java
@@ -10,7 +10,8 @@ public class Arraycode7
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter the string");
 		String str=sc.nextLine(); 
-		
+	    String  str_tmp="";
+
 		int count=0;
 		for(int i=0;i<str.length();i++)
 		{
@@ -23,5 +24,26 @@ public class Arraycode7
 		
 	}
 	System.out.println("word count : "+count);
-} 
+
+String arr[]=new String[count];
+int tmp=0;
+for(int i=str.length()-1;i>=0;i--)
+{
+	if(str.charAt(i)!=' ')
+	{
+		str_tmp=str_tmp+str.charAt(i);
+	}
+	
+	else
+	{
+		arr[tmp]=str_tmp;
+		tmp++;
+		str_tmp="";
+	}
+	}
+for(int i=arr.length-1;i>=0;i--)
+{
+	System.out.print(arr[i]+" ");
+}
+}
 }
