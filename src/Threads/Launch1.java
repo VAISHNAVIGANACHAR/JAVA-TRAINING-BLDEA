a3cc1d914dc7d706276d275d10175b0fe285f6ca
SACHIN <SACHIN@LAPTOP-64EQKSKD>
2023-04-17 19:57:53 +0530

    implements runnable commit

--- src/Threads/Launch1.java
+++ src/Threads/Launch1.java
@@ -1,10 +1,10 @@
 package Threads;
 import java.util.Scanner;
-class Addition extends Thread
+class Addition2 implements Runnable
 {
 	public void run()
 	{
-		System.out.println("addition operation");
+		System.out.println("addition2 operation");
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter num1");
 		int num1=sc.nextInt();
@@ -12,15 +12,15 @@ class Addition extends Thread
 		int num2=sc.nextInt();
 		int res=num1+num2;
 		System.out.println(res);
-		System.out.println("addition completed");
-		System.out.println("printing operation");
+		System.out.println("addition2 completed");
+		System.out.println("printing2 operation");
 	}
 }
-	class Printing extends Thread
+	class Printing2 implements Runnable
 	{
 		public void run()
 		{
-			System.out.println("addition operation");
+			System.out.println("addition2 operation");
 			Scanner sc=new Scanner(System.in);
 			System.out.println("enter num1");
 			int num1=sc.nextInt();
@@ -28,15 +28,15 @@ class Addition extends Thread
 			int num2=sc.nextInt();
 			int res=num1+num2;
 			System.out.println(res);
-			System.out.println("addition completed");
-			System.out.println("printing operation");
+			System.out.println("addition2 completed");
+			System.out.println("printing2 operation");
 		}
 	}
-		class banking extends Thread
+		class banking2 implements Runnable
 		{
 			public void run()
 			{
-				System.out.println("addition operation");
+				System.out.println("addition2 operation");
 				Scanner sc=new Scanner(System.in);
 				System.out.println("enter num1");
 				int num1=sc.nextInt();
@@ -44,27 +44,31 @@ class Addition extends Thread
 				int num2=sc.nextInt();
 				int res=num1+num2;
 				System.out.println(res);
-				System.out.println("addition completed");
-				System.out.println("printing operation");
+				System.out.println("addition2 completed");
+				System.out.println("printing2 operation");
 			}
 
 		}
 
-public class Threadex2  
+public class Launch1  
 {
 	public static void main(String[] args)
 	{
 	System.out.println("main stared");
-	Addition ad=new Addition();
+	Addition2 ad=new Addition2();
 	System.out.println(ad);
-	Printing pr=new Printing();
+	Printing2 pr=new Printing2();
 	System.out.println(pr);
-	banking bn=new banking();
-	System.out.println(bn);
+	banking2 bn=new banking2();
+	System.out.println(bn); 
 	
-	ad.start();
-	pr.start();
-	bn.start();
+	Thread t1=new 	Thread(ad);
+	Thread t2=new 	Thread(pr);
+	Thread t3=new 	Thread(bn);
+	
+	t1.start();
+	t2.start();
+	t3.start();
 	System.out.println("main terminated");
 	}
 }
